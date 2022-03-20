f439eb754ad42bf567407656b6ed25b8b3775c48
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-20 21:33:01 +0000

    hack

--- examples/imagenet/input_pipeline.py
+++ examples/imagenet/input_pipeline.py
@@ -135,7 +135,7 @@ def create_split(dataset_builder, batch_size, train, dtype=tf.float32,
   options = tf.data.Options()
   options.experimental_threading.private_threadpool_size = 48
   if aug is not None and aug.torchvision:
-    options.experimental_threading.private_threadpool_size = 2
+    options.experimental_threading.private_threadpool_size = 16
   ds = ds.with_options(options)
 
   if cache:
