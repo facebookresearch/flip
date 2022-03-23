23c3caa545f7134fbbfe56ff541aed300b2d8eeb
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-23 02:56:20 +0000

    script

--- examples/imagenet/run_staging.sh
+++ examples/imagenet/run_staging.sh
@@ -1,5 +1,5 @@
 # VM_NAME=kmh-tpuvm-v3-128
-VM_NAME=kmh-tpuvm-v3-128-2
+VM_NAME=kmh-tpuvm-v3-128
 echo $VM_NAME
 # REPO=https://github.com/google/flax
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
@@ -9,7 +9,7 @@ salt=`head /dev/urandom | tr -dc a-z0-9 | head -c8`
 
 CONFIG=tpu_vit_base
 # JOBNAME=flax/$(date +%Y%m%d_%H%M)_${salt}_${CONFIG}_cjit0.4MyV4BriFloat_lb0.1_cropv4_exwd_initv2_rsinit_torchv
-JOBNAME=flax/$(date +%Y%m%d_%H%M)_${salt}_${CONFIG}_cjit0.4MyDBGNOsat_lb0.1_cropv4_exwd_initv2_rsinit_NOtorchv
+JOBNAME=flax/$(date +%Y%m%d_%H%M)_${salt}_${CONFIG}_cjit0.4MyDBG_lb0.1_cropv4_exwd_initv2_rsinit_NOtorchv
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/home/${USER}/logs/${JOBNAME}
