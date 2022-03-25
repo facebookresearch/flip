f2055ccad9fddec7dec2646e5c1efa19b0adfc68
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-25 06:36:16 +0000

    gitignore

--- run_script.sh
+++ run_script.sh
@@ -4,7 +4,7 @@ rm -rf imagenet_tpu
 export TFDS_DATA_DIR=gs://kmh-gcp/tensorflow_datasets
 
 python3 main.py \
-    --workdir=./imagenet_tpu \
+    --workdir=./tmp \
     --config=configs/tpu_vit_dbg.py \
     --config.batch_size=1024 \
     --config.model.transformer.num_layers=12 \
