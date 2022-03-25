4971467b7f900b3ab8943d0b84606f8222f451a6
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-25 06:27:01 +0000

    clean up

--- run_env.sh
+++
@@ -1,6 +0,0 @@
-# pip install 'jax[tpu]>=0.2.21' -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-# pip install --upgrade clu
-
-
-## DO NOT run this
-# pip install -r requirements.txt
