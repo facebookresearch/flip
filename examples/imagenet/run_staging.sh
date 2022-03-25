dba947ab461dde32fc0b8cefd4ddcac316a66274
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-25 00:46:15 +0000

    script

--- examples/imagenet/run_staging.sh
+++ examples/imagenet/run_staging.sh
@@ -1,5 +1,5 @@
 # VM_NAME=kmh-tpuvm-v3-128
-VM_NAME=kmh-tpuvm-v3-128
+VM_NAME=kmh-tpuvm-v3-128-2
 echo $VM_NAME
 # REPO=https://github.com/google/flax
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
@@ -7,7 +7,7 @@ BRANCH=main
 
 salt=`head /dev/urandom | tr -dc a-z0-9 | head -c8`
 
-ep=300
+ep=100
 CONFIG=tpu_vit_base
 # JOBNAME=flax/$(date +%Y%m%d_%H%M)_${salt}_${CONFIG}_cjit0.4dbg_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup
 JOBNAME=flax/$(date +%Y%m%d_%H%M)_${salt}_${CONFIG}_${ep}ep_autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup
