09d21d1f407605134b5e515459843c9322da65db
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-28 21:25:13 +0000

    script

--- run_staging.sh
+++ run_staging.sh
@@ -1,20 +1,20 @@
 # VM_NAME=kmh-tpuvm-v3-128-2
-VM_NAME=kmh-tpuvm-v3-256-4
+VM_NAME=kmh-tpuvm-v3-256-3
 echo $VM_NAME
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
 BRANCH=main
 
 # salt=`head /dev/urandom | tr -dc a-z0-9 | head -c8`
 
-ep=100
-ema=0.99
+ep=200
+ema=0.9999
 batch=4096
 mutype=bfloat16
 
 
-CONFIG=cfg_vit_large
+CONFIG=cfg_vit_huge
 # pytorch_recipe: _autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup_minlr
-JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_pytorch_recipe_YESema${ema}ev2_batch${batch}_profmem_mu${mutype}_adamwutil_donate_inittpu_optema_sanity
+JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_pytorch_recipe_YESema${ema}ev2_batch${batch}_profmem_mu${mutype}_adamwutil_donate_inittpu_optema
 
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
@@ -46,7 +46,7 @@ python3 main.py \
     --workdir=$WORKDIR \
     --config=configs/$CONFIG.py \
     --config.batch_size=${batch} \
-    --config.log_every_steps=1 \
+    --config.log_every_steps=100 \
     --config.num_epochs=${ep} \
     --config.ema_decay=${ema} \
     --config.ema=True \
