bd4e37560c2258c5eb221cd2f5f66fd27459c0d9
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-28 04:10:13 +0000

    script

--- configs/cfg_vit_large.py
+++ configs/cfg_vit_large.py
@@ -42,4 +42,7 @@ def get_config():
   config.model.transformer.dropout_rate = 0.0
   config.model.transformer.droppath_rate = 0.2
 
+  # opt config
+  config.opt_mu_dtype = 'bfloat16'  # bfloat16, float32
+
   return config
--- run_staging.sh
+++ run_staging.sh
@@ -1,5 +1,5 @@
 # VM_NAME=kmh-tpuvm-v3-128-2
-VM_NAME=kmh-tpuvm-v3-256-3
+VM_NAME=kmh-tpuvm-v3-256-4
 echo $VM_NAME
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
 BRANCH=main
@@ -10,7 +10,7 @@ ep=200
 ema=0.9999
 batch=4096
 
-CONFIG=cfg_vit_huge
+CONFIG=cfg_vit_large
 # pytorch_recipe: _autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup_minlr
 JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_pytorch_recipe_YESema${ema}ev2_batch${batch}_profmem_mubf16_myadam
 
