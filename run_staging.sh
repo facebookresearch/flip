3fb15000b24ac16c4a35b00c82e27ad4b001bd62
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-28 18:54:05 +0000

    script

--- run_staging.sh
+++ run_staging.sh
@@ -1,4 +1,4 @@
-VM_NAME=kmh-tpuvm-v3-128-1
+VM_NAME=kmh-tpuvm-v3-128-2
 # VM_NAME=kmh-tpuvm-v3-256-4
 echo $VM_NAME
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
@@ -14,7 +14,7 @@ mutype=bfloat16
 
 CONFIG=cfg_vit_base
 # pytorch_recipe: _autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup_minlr
-JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_pytorch_recipe_YESema${ema}ev2_batch${batch}_profmem_mu${mutype}_adamwutil_donate_inittpu_sanity
+JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_pytorch_recipe_YESema${ema}ev2_batch${batch}_profmem_mu${mutype}_adamwutil_donate_inittpu_optema_sanity
 
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
