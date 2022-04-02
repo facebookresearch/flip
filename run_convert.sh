cb09116cf7c2d2340b6a58eb9837bc6dd28e1da1
KaimingHe <kmh.kaiminghe@gmail.com>
2022-04-02 17:50:36 +0000

    script

--- run_convert.sh
+++ run_convert.sh
@@ -1,5 +1,9 @@
 CONFIG=cfg_vit_large
-CHKPT_DIR='/checkpoint/kaiminghe/converted/2021-10-26-03-09-46-v3-128-mb4096-epo800-PMAEp16-ViTLarge-lr1e-4-wd5e-2-warm40-mask0.75-pred8d512-exNB-msaLNmlpLNeLNpLNkBN0-1view-NOrelpos-abspos-clstoken-qkv-NOlayerscale-LNtgt/pretrained_lastnorm_tf2pt.pth'
+# CHKPT_DIR='/checkpoint/kaiminghe/converted/2021-10-26-03-09-46-v3-128-mb4096-epo800-PMAEp16-ViTLarge-lr1e-4-wd5e-2-warm40-mask0.75-pred8d512-exNB-msaLNmlpLNeLNpLNkBN0-1view-NOrelpos-abspos-clstoken-qkv-NOlayerscale-LNtgt/pretrained_lastnorm_tf2pt.pth'
+CHKPT_DIR='/checkpoint/kaiminghe/converted/2022-04-02-03-06-12-v3-128-mb4096-epo200-PMAEp16-ViTLarge-lr1.5e-4-wd5e-2-warm40-mask0.75-pred8d512-exNB-msaLNmlpLNeLNpLNkBN0-1view-NOrelpos-abspos-clstoken-qkv-NOlayerscale-LNtgteps1e-6/pretrained_lastnorm_tf2pt.pth'
+
+
+
 GCP_CHKPT_DIR=gs://kmh-gcp/from_pytorch$CHKPT_DIR
 
 # gsu cp $CHKPT_DIR $GCP_CHKPT_DIR
