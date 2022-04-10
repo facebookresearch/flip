f7a07455b4a18bd8b28c43bf935dda77f7af72db
KaimingHe <kmh.kaiminghe@gmail.com>
2022-04-10 23:20:24 +0000

    script

--- run_staging.sh
+++ run_staging.sh
@@ -1,5 +1,5 @@
 # VM_NAME=kmh-tpuvm-v3-128-1
-VM_NAME=kmh-tpuvm-v3-256-3
+VM_NAME=kmh-tpuvm-v3-256-4
 echo $VM_NAME
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
 BRANCH=main
@@ -21,7 +21,7 @@ source scripts/select_chkpt_${vitsize}.sh
 name=`basename ${PRETRAIN_DIR}`
 
 # pytorch_recipe (pyre): _autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup_minlr
-JOBNAME=flax/${name}_finetune/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_FT_b${batch}_lr${lr}_lrd${lrd}_${cls}_hinit${head_init}_b0.999_32mixup_32cutmix_aaRand_NOerase_warmlr_NOencnorm_shf512b_fullevsp
+JOBNAME=flax/${name}_finetune/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_FT_b${batch}_lr${lr}_lrd${lrd}_${cls}_hinit${head_init}_b0.999_NOmixup_NOcutmix_NOaa_YESerase_warmlr_NOencnorm_shf512b_fullevsp
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/home/${USER}/logs/${JOBNAME}
@@ -65,12 +65,12 @@ python3 main.py \
     --config.init_backend=tpu \
     --config.model.classifier=${cls} \
     --config.rescale_head_init=${head_init} \
-    --config.aug.mix.mixup=True \
-    --config.aug.mix.cutmix=True \
+    --config.aug.mix.mixup=False \
+    --config.aug.mix.cutmix=False \
     --config.aug.mix.batch_size=32 \
     --config.aug.mix.lambda_elementwise=False \
-    --config.aug.autoaug=randaug \
-    --config.aug.randerase.on=False \
+    --config.aug.autoaug=None \
+    --config.aug.randerase.on=True \
     --config.warmup_abs_lr=1e-6 \
 " 2>&1 | tee $LOGDIR/finetune.log
 
