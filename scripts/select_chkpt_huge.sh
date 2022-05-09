355e5c6b7ab40ddf338f281407649e06c43a8e5e
KaimingHe <kmh.kaiminghe@gmail.com>
2022-05-09 04:49:29 +0000

    script

--- scripts/select_chkpt_huge.sh
+++ scripts/select_chkpt_huge.sh
@@ -6,4 +6,5 @@
 # PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220425_070228_kmh-tpuvm-v3-256-4_cfg_mae_huge_maetf_3200ep_b4096_lr1.0e-4_TorchLoader_wseed100'
 
 # 0.85
-PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220504_011721_kmh-tpuvm-v3-256-4_cfg_mae_huge_maetf_1600ep_b4096_lr1.0e-4_mask0.85_TorchLoader_wseed100'
+# PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220504_011721_kmh-tpuvm-v3-256-4_cfg_mae_huge_maetf_1600ep_b4096_lr1.0e-4_mask0.85_TorchLoader_wseed100'
+PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220502_060255_kmh-tpuvm-v3-256-3_cfg_mae_huge_maetf_6400ep_b4096_lr1.0e-4_mask0.85_TorchLoader_wseed100'
