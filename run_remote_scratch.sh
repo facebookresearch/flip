8da41d6fe5e82dc2a6e8e1f284818257279a57a4
KaimingHe <kmh.kaiminghe@gmail.com>
2022-05-19 06:13:37 +0000

    script

--- run_remote_scratch.sh
+++ run_remote_scratch.sh
@@ -10,6 +10,8 @@ warm=20
 dp=0.2
 beta2=0.95
 
+partitions=4
+
 vitsize=large
 CONFIG=cfg_vit_${vitsize}
 
@@ -19,7 +21,7 @@ CONFIG=cfg_vit_${vitsize}
 
 # finetune_pytorch_recipe (ftpy): lb0.1_b0.999_cropv4_exwd_initv2_headinit0.001_tgap_dp_mixup32_cutmix32_noerase_warmlr_minlr_autoaug
 # finetune_torch_loader (fttl): randaugv2erase_TorchLoader
-JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_scratch_${VM_NAME}_${CONFIG}_${ep}ep_fttl_b${batch}_wd${wd}_lr${lr}_lrd${lrd}_dp${dp}_warm${warm}_s${seed}_beta${beta2}_fnpartition1_hwrng_log
+JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_scratch_${VM_NAME}_${CONFIG}_${ep}ep_fttl_b${batch}_wd${wd}_lr${lr}_lrd${lrd}_dp${dp}_warm${warm}_s${seed}_beta${beta2}_p${partitions}_hwrng_log
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/kmh_data/logs/${JOBNAME}
@@ -66,6 +68,7 @@ python3 main.py \
     --config.seed_pt=${seed} \
     --config.model.transformer.torch_qkv=False \
     --config.model.classifier=token \
+    --config.partitioning.num_partitions=${partitions} \
 2>&1 | tee $LOGDIR/finetune_\$SSH_ID.log
 " 2>&1 | tee $LOGDIR/finetune.log
 
--- run_script.sh
+++ run_script.sh
@@ -5,7 +5,7 @@ rm -rf tmp
 export TFDS_DATA_DIR=gs://kmh-gcp/tensorflow_datasets
 python3 main.py \
     --workdir=./tmp \
-    --config=configs/cfg_vit_dbg.py \
+    --config=configs/cfg_vit_large.py \
     --config.batch_size=32 \
     --config.log_every_steps=10 \
     --config.num_epochs=1 \
@@ -21,7 +21,7 @@ python3 main.py \
     --config.eval_only=False \
     --config.model.classifier=token \
     --config.learning_rate_decay=1. \
-    --config.partitioning.num_partitions=1 \
+    --config.partitioning.num_partitions=4 \
 
     # --config.pretrain_dir='gs://kmh-gcp/from_pytorch/checkpoint/kaiminghe/converted/2021-10-26-22-16-05-v3-128-mb4096-epo1600-PMAEp16-ViTLarge-lr1e-4-wd5e-2-warm40-mask0.75-pred8d512-exNB-msaLNmlpLNeLNpLNkBN0-1view-NOrelpos-abspos-clstoken-qkv-NOlayerscale-LNtgt-resume3_convert_pt2jax'
 
