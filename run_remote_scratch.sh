1d8da6f98538e299c1a7da26a490d16556c615ed
KaimingHe <kmh.kaiminghe@gmail.com>
2022-05-22 00:22:14 +0000

    add partition=16 hack

--- run_remote_scratch.sh
+++ run_remote_scratch.sh
@@ -10,9 +10,9 @@ warm=20
 dp=0.2
 beta2=0.95
 
-partitions=16
+partitions=32
 
-vitsize=dbg
+vitsize=large
 CONFIG=cfg_vit_${vitsize}
 
 # source scripts/select_chkpt_${vitsize}.sh
@@ -21,7 +21,7 @@ CONFIG=cfg_vit_${vitsize}
 
 # finetune_pytorch_recipe (ftpy): lb0.1_b0.999_cropv4_exwd_initv2_headinit0.001_tgap_dp_mixup32_cutmix32_noerase_warmlr_minlr_autoaug
 # finetune_torch_loader (fttl): randaugv2erase_TorchLoader
-JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_scratch_${VM_NAME}_${CONFIG}_${ep}ep_fttl_b${batch}_wd${wd}_lr${lr}_lrd${lrd}_dp${dp}_warm${warm}_s${seed}_beta${beta2}_p${partitions}_dbgp16
+JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_scratch_${VM_NAME}_${CONFIG}_${ep}ep_fttl_b${batch}_wd${wd}_lr${lr}_lrd${lrd}_dp${dp}_warm${warm}_s${seed}_beta${beta2}_p${partitions}_dbg
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/kmh_data/logs/${JOBNAME}
